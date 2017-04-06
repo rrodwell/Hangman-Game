d8ac3dcc66cd24753d278fc1501ff51ff6ef9322
Ryan Rodwell <ryanrodwell@gmail.com>
2017-04-06 18:36:59 -0400

    fixing syntax

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Array of words to guess from
+var accFootballTeams = ["clemson", "gatech","vatech","virginia","duke","louisville", "pittsburgh", "floridastate","bostoncollege", "wakeforest","syracuse","miami","ncstate","northcarolina"]
+var 
+
+
+
+
+
+
+
